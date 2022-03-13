b044c74b3179148647265c4919aa21b19833d937
benjjo <benjjo@gmail.com>
2022-03-14 09:23:53 +1100

    cleaned up the lols

--- main.py
+++ main.py
@@ -8,25 +8,28 @@ def fpv_life():
     This is probably the most useful thing on the internet and its here, free.
     You're welcome.
     """
-    adjective = ["BarrelRolling", "YawSpinning", "RubiksCubed", "InvertedYawSpinning", "JuicyFlicking", "Stalling",
-                 "ThrowBacking", "VannyRolling", "KnifeEdging", "PowerLooping", "Orbiting", "Split-S'n",
-                 "Immelmanning", "MattyFlipping", "TrippySpinning", "OfficeWindowDiving", "BackwardDiving",
+    adjective = ["BarrelRolling", "YawSpinning", "RubiksCubing", "InvertedYawSpinning", "JuicyFlicking", "Stalling",
+                 "ThrowBacking", "VannyRolling", "KnifeEdging", "PowerLooping", "Orbiting", "Split-S'n", "SIM-Racing",
+                 "Immelmanning", "MattyFlipping", "TrippySpinning", "OfficeDiving", "BackwardDiving", "StockFlying"
                  "WallBonking", "GimbalMashing", "HeadlessModing", "LiPoSucking", "PayloadDropping", "Inverted",
                  "AcroRipping", "Brushless", "Freestyling", "Chicaning", "GapDiving", "PylonHugging", "CorkScrewing",
                  "BandoFenceJumping", "DriftChasing", "WallRiding", "FlagShredding", "KissDevoted", "ButterFlying",
                  "OldSchool", "BetaFlightLoving", "WhoopKing", "StockFlying", "BridgeDiving", "Toothpicked", "Jello",
                  "GateSmashing", "TowerSmashing", "LadderLooping", "RipSeshing", "SlalomSmashing", "ThrottleTrimming",
-                 "MotorSmoking", "ESC_Burning", "LiPoKilling", "PowerHungry"]
-    noun = ["DroneTemplePilot", "SlipStreamer", "Ghost", "Thor", "SpaceCaptain", "Lord", "Mong", "Jedi",
-            "King", "TheAlmighty", "God", "BardwellKnowItAll", "SpacePilot",
-            "SkyLord", "DemiGod", "Shyamalan", "SacrificialGoat", "Beast", "Jeebus",
+                 "MotorSmoking", "ESC-Burning", "LiPoKilling", "PowerHungry", "TrainChasing", "Overpowered"]
+    noun = ["DroneTemplePilot", "SlipStreamer", "Ghost", "Thor", "SpaceCaptain", "Lord", "Mong", "Jedi", "StarLord"
+            "King", "AlmightyDroneGod", "God", "BardwellKnowItAll", "SpacePilot", "StingyFanboy", "NurkFanboy"
+            "SkyLord", "DemiGod", "Shyamalan", "SacrificialGoat", "Beast", "Jeebus", "ELRS-Fanboy", "PID-Master"
             "Daddy", "MamasBoy", "Goblin", "PantyDropper", "Master", "Gimp", "ArchNemesis", "Brute", "Satan",
-            "Fiend", "MetaGod", "Deity", "Monster", "Villain", "Demon", "Bohdi",
-            "Succubus", "Gandhi", "Devil", "MalignantSkyGod", "Idol", "Imp", "GodOfWar",
-            "WarLord", "KingKong", "MoFo", "BossMan", "Captain", "Maniac", "SimulatorGod",
-            "Warrior", "Noob", "SpaceCadet", "PocketRocket", "HellBoi", "DRL_Champion", "SimHero", "Samurai",
-            "AutoPilot", "PropBurner", "StarLord", "Spotter", "TBS_Fanboy", "DJI_Fanboy", "DigitalSnob",
-            "GeoFenceHacker", "ELRS_Fanboy"]
+            "Fiend", "MetaGod", "Deity", "Monster", "Villain", "Demon", "Bohdi", "LineMaster", "GeoFenceHacker"
+            "Succubus", "Gandhi", "Devil", "MalignantSkyGod", "Idol", "Imp", "GodOfWar", "DJI-Fanboy", "DigitalSnob",
+            "WarLord", "KingKong", "MoFo", "BossMan", "Captain", "Maniac", "SimulatorGod", "Spotter", "TBS-Fanboy"
+            "Warrior", "Noob", "SpaceCadet", "PocketRocket", "HellBoi", "DRL-Champion", "SimHero", "Samurai",
+            "AutoPilot", "PropBurner", "ESC-Burner"]
+
+    # Remove the duplicates - coz im lazy.
+    adjective = list(set(adjective))
+    noun = list(set(noun))
 
     return adjective[randrange(0, len(adjective))] + noun[randrange(0, len(noun))] + 'FPV'
 
